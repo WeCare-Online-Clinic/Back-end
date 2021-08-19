4fc914d997291275b0d461c182cf14139d30b3a1
Aveeshajay <aveeeshajayawardena12@gmail.com>
2021-08-18 22:41:08 -0700

    Headnurse -patient manage

---
+++ src/main/java/wecare/backend/controller/PatientController.java
@@ -0,0 +1,60 @@
+package wecare.backend.controller;
+
+
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+import java.util.Optional;
+
+import javax.mail.MessagingException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import wecare.backend.exception.UserCollectionException;
+import wecare.backend.model.Doctor;
+import wecare.backend.model.Patient;
+import wecare.backend.service.PatientService;
+
+@RestController
+@RequestMapping(value = "wecare")
+@CrossOrigin(origins="http://localhost:3000")
+public class PatientController {
+
+	@Autowired 
+	private PatientService patientService;
+	
+	@PostMapping("/addPatient")
+	public ResponseEntity<Patient> addPatient(@RequestBody Patient patient) throws UserCollectionException, MessagingException, UnsupportedEncodingException {
+		return new ResponseEntity<Patient>(patientService.addPatient(patient),HttpStatus.OK);
+	}
+	
+	@GetMapping("/getPatient")
+	public List<Patient> getPatient(){
+		List<Patient> patient=PatientService.getAllPatient();
+		return patient;
+	}
+	
+	
+	@GetMapping("/getPatientProfile/{id}") 
+	public  List<Patient> getPatientProfileById(@PathVariable Integer id){
+		List<Patient> patient = patientService.getPatientProfileById(id);
+		return patient;
+		
+	}
+	@GetMapping("/getPatientProfileByName/{name}")
+	public List<Patient> getPatientProfileByName(@PathVariable String name){
+		List<Patient> patient=patientService.getPatientProfileByName(name);
+		return patient;
+		
+	}
+	
+	
+}
--- src/main/java/wecare/backend/repository/PatientRepository.java
+++ src/main/java/wecare/backend/repository/PatientRepository.java
@@ -1,10 +1,17 @@
 package wecare.backend.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import wecare.backend.model.Patient;
 
 @Repository
 public interface PatientRepository extends JpaRepository<Patient, Integer> {
+	Patient findByEmail(String email);
+	Optional <Patient> findById(Integer id);
 
 }
