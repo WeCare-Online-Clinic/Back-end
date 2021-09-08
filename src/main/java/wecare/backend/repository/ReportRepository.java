cacfbe34fe5290781334103fecbf20609a86e255
madhavi125 <msellahannadi@gmail.com>
2021-09-08 08:32:56 +0530

    test repository

--- src/main/java/wecare/backend/repository/ReportRepository.java
+++ src/main/java/wecare/backend/repository/ReportRepository.java
@@ -1,6 +1,4 @@
 package wecare.backend.repository;
-
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,20 +6,22 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+import wecare.backend.model.Nurse;
 import wecare.backend.model.Report;
 
 @Repository
 public interface ReportRepository extends JpaRepository<Report, Integer> {
-
-    Report findByEmail(String email);
-
-    @Query("SELECT n FROM Report n WHERE  n.status=true")
-    List<Report> findAllReport();
-
-    @Query("SELECT n FROM Report n WHERE n.name LIKE %:name% AND n.status=true")
-    List<Report> findByFirstNameLike(@Param("name") String name);
-
-    @Query("SELECT n FROM Report n WHERE n.id = :id AND n.status=true")
-    List<Report> getReportProfileById(@Param("id") Integer id);
+	Report findByEmail(String email);
+	Optional <Report> findById(Integer id);
+	
+	List<Report> findByClinicId(Integer clinicId);
+	
+	@Query("SELECT r FROM Report r WHERE r.name LIKE %:name% ")
+	List<Report> findByFirstNameLike(@Param("name") String name);
+	
+	@Query("SELECT r FROM Report r WHERE r.id = :id")
+	List<Report> getReportProfileById(@Param("id") Integer id);
 
 }
